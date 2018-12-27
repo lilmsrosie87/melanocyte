36ed6a12f097668e63e46f0cd9eb5d96cd2f6b32
Roslyn Harris <rharris0113@gmail.com>
2018-12-26 21:24:25 -0800

    created constructor notation with many objects

---
+++ js/array.js
@@ -0,0 +1,32 @@
+function getThatSwatch () {
+	document.write('Swatches for deeper complexions! Coming Soon');
+}
+
+// http://w3schools.invisionzone.com/topic/190-display-images-with-javascript/
+
+
+
+//constructor notation with many objects 
+
+ function foundation(image, name, brand, price) {
+	 this.image = image
+	 this.name = name;
+	 this.brand = brand;
+	 this.price = price;
+
+	 
+var lipstick = new product(); {
+	melanin.image = 'img/caramelSwatch.jpeg';
+	melanin.name = 'Caramel';
+	melanin.brand = 'Revlon';
+	melanin.price = '$10.99'; 
+	
+var melanin = new product(); {
+	melanin.image = 'img/pecanSwatch.jpeg';
+	melanin.name = 'Pecan';
+	melanin.brand = 'Revlon';
+	melanin.price = '$10.99';
+	
+	document.body.appendChild(melanin)
+	
+}
