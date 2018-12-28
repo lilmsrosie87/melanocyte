1a9587329e14d4fc9e0d0f63a49b2f3197cf91ff
Roslyn Harris <rharris0113@gmail.com>
2018-12-27 20:47:15 -0800

    attempting to display new products in HTML. Still not working

--- js/array.js
+++ js/array.js
@@ -1,9 +1,9 @@
 var x = document.createElement("DD");
-var y = document.createTextNode("It's working");
+// var y = document.createTextNode("It's working");
 
-function getThatSwatch () {
-	document.write('Swatches for deeper complexions! Coming Soon');
-}
+// function getThatSwatch () {
+	// document.write('Swatches for deeper complexions! Coming Soon');
+// }
 
 // http://w3schools.invisionzone.com/topic/190-display-images-with-javascript/
 
@@ -11,25 +11,25 @@ function getThatSwatch () {
 
 //constructor notation with many objects 
 
- // function foundation(image, name, brand, price) {
-	 // this.image = image
-	 // this.name = name;
-	 // this.brand = brand;
-	 // this.price = price;
+  function foundation(image, name, brand, price) {
+	this.image = image
+	this.name = name;
+	this.brand = brand;
+	this.price = price;
 
 	 
-// var lipstick = new product(); {
-	// melanin.image = 'img/caramelSwatch.jpeg';
-	// melanin.name = 'Caramel';
-	// melanin.brand = 'Revlon';
-	// melanin.price = '$10.99'; 
+var lipstick = new product(); {
+	 melanin.image = 'img/caramelSwatch.jpeg';
+	 melanin.name = 'Caramel';
+	 melanin.brand = 'Revlon';
+	melanin.price = '$10.99'; 
 	
-// var melanin = new product(); {
-	// melanin.image = 'img/pecanSwatch.jpeg';
-	// melanin.name = 'Pecan';
-	// melanin.brand = 'Revlon';
-	// melanin.price = '$10.99';
+ var melanin = new product(); {
+	melanin.image = 'img/pecanSwatch.jpeg';
+	melanin.name = 'Pecan';
+	melanin.brand = 'Revlon';
+	melanin.price = '$10.99';
 	
-// }
+ }
 
-	// document.getElementById("
+	 document.getElementByTagName("DD");
